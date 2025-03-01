3bab8a9709f79ea8e76dd61a448e8006e981b544
vapelite <yhits10inches@gmail.com>
2025-03-01 12:58:52 -0600

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -6010,70 +6010,82 @@ run(function()
 end)
 	
 run(function()
-	local Blink
-	local Type
-	local AutoSend
-	local AutoSendLength
-	local oldphys, oldsend
-	
-	Blink = vape.Categories.Utility:CreateModule({
-		Name = 'Blink',
-		Function = function(callback)
-			if callback then
-				local teleported
-				Blink:Clean(lplr.OnTeleport:Connect(function()
-					setfflag('S2PhysicsSenderRate', '15')
-					setfflag('DataSenderRate', '60')
-					teleported = true
-				end))
-	
-				repeat
-					local physicsrate, senderrate = '0', Type.Value == 'All' and '-1' or '60'
-					if AutoSend.Enabled and tick() % (AutoSendLength.Value + 0.1) > AutoSendLength.Value then
-						physicsrate, senderrate = '15', '60'
-					end
-	
-					if physicsrate ~= oldphys or senderrate ~= oldsend then
-						setfflag('S2PhysicsSenderRate', physicsrate)
-						setfflag('DataSenderRate', senderrate)
-						oldphys, oldsend = physicsrate, oldsend
-					end
-					
-					task.wait(0.03)
-				until (not Blink.Enabled and not teleported)
-			else
-				if setfflag then
-					setfflag('S2PhysicsSenderRate', '15')
-					setfflag('DataSenderRate', '60')
-				end
-				oldphys, oldsend = nil, nil
-			end
-		end,
-		Tooltip = 'Chokes packets until disabled.'
-	})
-	Type = Blink:CreateDropdown({
-		Name = 'Type',
-		List = {'Movement Only', 'All'},
-		Tooltip = 'Movement Only - Only chokes movement packets\nAll - Chokes remotes & movement'
-	})
-	AutoSend = Blink:CreateToggle({
-		Name = 'Auto send',
-		Function = function(callback)
-			AutoSendLength.Object.Visible = callback
-		end,
-		Tooltip = 'Automatically send packets in intervals'
-	})
-	AutoSendLength = Blink:CreateSlider({
-		Name = 'Send threshold',
-		Min = 0,
-		Max = 1,
-		Decimal = 100,
-		Darker = true,
-		Visible = false,
-		Suffix = function(val)
-			return val == 1 and 'second' or 'seconds'
-		end
-	})
+    local Blink
+    local Type
+    local AutoSend
+    local AutoSendLength
+    local oldphys, oldsend
+    local keepAliveEvent = Instance.new('BindableEvent')
+
+    Blink = vape.Categories.Utility:CreateModule({
+        Name = 'Blink',
+        Function = function(callback)
+            if callback then
+                local teleported
+                Blink:Clean(lplr.OnTeleport:Connect(function()
+                    setfflag('S2PhysicsSenderRate', '15')
+                    setfflag('DataSenderRate', '60')
+                    teleported = true
+                end))
+
+                Blink:Clean(runService.Heartbeat:Connect(function()
+                    keepAliveEvent:Fire()
+                end))
+
+                repeat
+                    local physicsrate, senderrate = '0', Type.Value == 'All' and '-1' or '60'
+                    if AutoSend.Enabled and tick() % (AutoSendLength.Value + 0.1) > AutoSendLength.Value then
+                        physicsrate, senderrate = '15', '60'
+                    end
+
+                    if physicsrate ~= oldphys or senderrate ~= oldsend then
+                        setfflag('S2PhysicsSenderRate', physicsrate)
+                        setfflag('DataSenderRate', senderrate)
+                        oldphys, oldsend = physicsrate, oldsend
+                    end
+
+                    task.wait(0.03)
+                until (not Blink.Enabled and not teleported)
+            else
+                if setfflag then
+                    setfflag('S2PhysicsSenderRate', '15')
+                    setfflag('DataSenderRate', '60')
+                end
+                oldphys, oldsend = nil, nil
+            end
+        end,
+        Tooltip = 'Chokes packets until disabled.'
+    })
+    Type = Blink:CreateDropdown({
+        Name = 'Type',
+        List = {'Movement Only', 'All'},
+        Tooltip = 'Movement Only - Only chokes movement packets\nAll - Chokes remotes & movement'
+    })
+    AutoSend = Blink:CreateToggle({
+        Name = 'Auto send',
+        Function = function(callback)
+            AutoSendLength.Object.Visible = callback
+        end,
+        Tooltip = 'Automatically send packets in intervals'
+    })
+    AutoSendLength = Blink:CreateSlider({
+        Name = 'Send threshold',
+        Min = 0,
+        Max = 1,
+        Decimal = 100,
+        Darker = true,
+        Visible = false,
+        Suffix = function(val)
+            return val == 1 and 'second' or 'seconds'
+        end
+    })
+
+    -- Keep-alive functionality to prevent disconnection
+    Blink:Clean(runService.Heartbeat:Connect(function()
+        if Blink.Enabled then
+            keepAliveEvent:Fire()
+        end
+    end))
 end)
 	
 run(function()
