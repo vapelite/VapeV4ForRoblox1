f308f0e7dbf4fa08b3b330ab6acf9221aa39bd3d
vapelite <yhits10inches@gmail.com>
2025-03-01 13:33:48 -0600

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -4527,47 +4527,48 @@ run(function()
 end)
 	
 run(function()
-	local AutoPlay
-	local Random
-	
-	local function isEveryoneDead()
-		return #bedwars.Store:getState().Party.members <= 0
-	end
-	
-	local function joinQueue()
-		if not bedwars.Store:getState().Game.customMatch and bedwars.Store:getState().Party.leader.userId == lplr.UserId and bedwars.Store:getState().Party.queueState == 0 then
-			if Random.Enabled then
-				local listofmodes = {}
-				for i, v in bedwars.QueueMeta do
-					if not v.disabled and not v.voiceChatOnly and not v.rankCategory then 
-						table.insert(listofmodes, i) 
-					end
-				end
-				bedwars.QueueController:joinQueue(listofmodes[math.random(1, #listofmodes)])
-			else
-				bedwars.QueueController:joinQueue(store.queueType)
-			end
-		end
-	end
-	
-	AutoPlay = vape.Categories.Utility:CreateModule({
-		Name = 'AutoPlay',
-		Function = function(callback)
-			if callback then
-				AutoPlay:Clean(vapeEvents.EntityDeathEvent.Event:Connect(function(deathTable)
-					if deathTable.finalKill and deathTable.entityInstance == lplr.Character and isEveryoneDead() and store.matchState ~= 2 then
-						joinQueue()
-					end
-				end))
-				AutoPlay:Clean(vapeEvents.MatchEndEvent.Event:Connect(joinQueue))
-			end
-		end,
-		Tooltip = 'Automatically queues after the match ends.'
-	})
-	Random = AutoPlay:CreateToggle({
-		Name = 'Random',
-		Tooltip = 'Chooses a random mode'
-	})
+    local AutoPlay
+    local Random
+    
+    local function isEveryoneDead()
+        return #bedwars.Store:getState().Party.members <= 0
+    end
+    
+    local function joinQueue()
+        if not bedwars.Store:getState().Game.customMatch and bedwars.Store:getState().Party.leader.userId == lplr.UserId and bedwars.Store:getState().Party.queueState == 0 then
+            if Random.Enabled then
+                local listofmodes = {}
+                for i, v in bedwars.QueueMeta do
+                    if not v.disabled and not v.voiceChatOnly and not v.rankCategory then 
+                        table.insert(listofmodes, i) 
+                    end
+                end
+                bedwars.QueueController:joinQueue(listofmodes[math.random(1, #listofmodes)])
+            else
+                bedwars.QueueController:joinQueue(store.queueType)
+            end
+        end
+    end
+    
+    AutoPlay = vape.Categories.Utility:CreateModule({
+        Name = 'AutoPlay',
+        Function = function(callback)
+            if callback then
+                joinQueue() -- Instantly play a random mode when turned on
+                AutoPlay:Clean(vapeEvents.EntityDeathEvent.Event:Connect(function(deathTable)
+                    if deathTable.finalKill and deathTable.entityInstance == lplr.Character and isEveryoneDead() and store.matchState ~= 2 then
+                        joinQueue()
+                    end
+                end))
+                AutoPlay:Clean(vapeEvents.MatchEndEvent.Event:Connect(joinQueue))
+            end
+        end,
+        Tooltip = 'Automatically queues after the match ends.'
+    })
+    Random = AutoPlay:CreateToggle({
+        Name = 'Random',
+        Tooltip = 'Chooses a random mode'
+    })
 end)
 	
 run(function()
