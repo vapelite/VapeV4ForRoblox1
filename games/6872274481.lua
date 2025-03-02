4c25af9b4d7dd1a3e796cd4cd02f0c8d2eb354ac
vapelite <yhits10inches@gmail.com>
2025-03-02 14:08:23 -0600

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -1296,116 +1296,7 @@ end)
 
 
 	
-run(function()
-    local AutoClicker
-    local CPS
-    local BlockCPS = {}
-    local Thread
-    local oldBlockPlace
-    local OldCps
 
-    local function AutoClick()
-        if Thread then
-            task.cancel(Thread)
-        end
-    
-        Thread = task.delay(0, function()  -- Start immediately
-            repeat
-                if not bedwars.AppController:isLayerOpen(bedwars.UILayers.MAIN) then
-                    local blockPlacer = bedwars.BlockPlacementController.blockPlacer
-                    if store.hand.toolType == 'block' and blockPlacer then
-                        local mouseinfo = blockPlacer.clientManager:getBlockSelector():getMouseInfo(0)
-                        if mouseinfo and mouseinfo.placementPosition == mouseinfo.placementPosition then
-                            task.spawn(blockPlacer.placeBlock, blockPlacer, mouseinfo.placementPosition)
-                        end
-                    elseif store.hand.toolType == 'sword' and bedwars.DaoController.chargingMaid == nil then
-                        bedwars.SwordController:swingSwordAtMouse()
-                    end
-                end
-    
-                task.wait(0)  -- No delay between clicks (math.huge equivalent)
-            until not AutoClicker.Enabled
-        end)
-    end
-    
-    AutoClicker = vape.Categories.Combat:CreateModule({
-        Name = 'AutoClicker',
-        Function = function(callback)
-            if callback then
-                oldBlockPlace = bedwars.BlockPlacementController.placeBlock
-                bedwars.BlockPlacementController.placeBlock = function(self, ...)
-                    self.lastPlace = tick()
-                    return oldBlockPlace(self, ...)
-                end
-
-                -- Ensure NoPlacementCps is always true
-                OldCps = PlacementCPS.BLOCK_PLACE_CPS
-                PlacementCPS.BLOCK_PLACE_CPS = math.huge
-
-                AutoClicker:Clean(inputService.InputBegan:Connect(function(input)
-                    if input.UserInputType == Enum.UserInputType.MouseButton1 then
-                        AutoClick()
-                    end
-                end))
-    
-                AutoClicker:Clean(inputService.InputEnded:Connect(function(input)
-                    if input.UserInputType == Enum.UserInputType.MouseButton1 and Thread then
-                        task.cancel(Thread)
-                        Thread = nil
-                    end
-                end))
-    
-                if inputService.TouchEnabled then
-                    pcall(function()
-                        AutoClicker:Clean(lplr.PlayerGui.MobileUI['2'].MouseButton1Down:Connect(AutoClick))
-                        AutoClicker:Clean(lplr.PlayerGui.MobileUI['2'].MouseButton1Up:Connect(function()
-                            if Thread then
-                                task.cancel(Thread)
-                                Thread = nil
-                            end
-                        end))
-                    end)
-                end
-            else
-                if Thread then
-                    task.cancel(Thread)
-                    Thread = nil
-                end
-                bedwars.BlockPlacementController.placeBlock = oldBlockPlace
-
-                -- Restore the original CPS when the module is disabled
-                if OldCps then
-                    PlacementCPS.BLOCK_PLACE_CPS = OldCps
-                end
-            end
-        end,
-        Tooltip = 'Hold attack button to automatically click'
-    })
-    CPS = AutoClicker:CreateTwoSlider({
-        Name = 'CPS',
-        Min = 1,
-        Max = 1e+100000000,
-        DefaultMin = 7,
-        DefaultMax = 7
-    })
-    AutoClicker:CreateToggle({
-        Name = 'Place Blocks',
-        Default = true,
-        Function = function(callback)
-            if BlockCPS.Object then
-                BlockCPS.Object.Visible = callback
-            end
-        end
-    })
-    BlockCPS = AutoClicker:CreateTwoSlider({
-        Name = 'Block CPS',
-        Min = 1,
-        Max = math.huge,  -- Set max to math.huge
-        DefaultMin = math.huge,  -- Set default to math.huge
-        DefaultMax = math.huge,  -- Set default to math.huge
-        Darker = true
-    })
-end)
 
 -- NoClickDelay Feature
 run(function()
@@ -8729,4 +8620,104 @@ run(function()
             end
         end
     })
+end)
+
+run(function()
+	local AutoClicker
+	local CPS
+	local BlockCPS = {}
+	local Thread
+	
+	local function AutoClick()
+		if Thread then
+			task.cancel(Thread)
+		end
+	
+		Thread = task.delay(1 / 7, function()
+			repeat
+				if not bedwars.AppController:isLayerOpen(bedwars.UILayers.MAIN) then
+					local blockPlacer = bedwars.BlockPlacementController.blockPlacer
+					if store.hand.toolType == 'block' and blockPlacer then
+						if PlacementCPS.BLOCK_PLACE_CPS == math.huge or (workspace:GetServerTimeNow() - bedwars.BlockCpsController.lastPlaceTimestamp) >= ((1 / 12) * 0.5) then
+							local mouseinfo = blockPlacer.clientManager:getBlockSelector():getMouseInfo(0)
+							if mouseinfo and mouseinfo.placementPosition == mouseinfo.placementPosition then
+								task.spawn(blockPlacer.placeBlock, blockPlacer, mouseinfo.placementPosition)
+							end
+						end
+					elseif store.hand.toolType == 'sword' and bedwars.DaoController.chargingMaid == nil then
+						bedwars.SwordController:swingSwordAtMouse()
+					end
+				end
+	
+				task.wait(1 / (store.hand.toolType == 'block' and BlockCPS or CPS).GetRandomValue())
+			until not AutoClicker.Enabled
+		end)
+	end
+	
+	AutoClicker = vape.Categories.Combat:CreateModule({
+		Name = 'AutoClicker',
+		Function = function(callback)
+			if callback then
+				AutoClicker:Clean(inputService.InputBegan:Connect(function(input)
+					if input.UserInputType == Enum.UserInputType.MouseButton1 then
+						AutoClick()
+					end
+				end))
+	
+				AutoClicker:Clean(inputService.InputEnded:Connect(function(input)
+					if input.UserInputType == Enum.UserInputType.MouseButton1 and Thread then
+						task.cancel(Thread)
+						Thread = nil
+					end
+				end))
+	
+				if inputService.TouchEnabled then
+					pcall(function()
+						AutoClicker:Clean(lplr.PlayerGui.MobileUI['2'].MouseButton1Down:Connect(AutoClick))
+						AutoClicker:Clean(lplr.PlayerGui.MobileUI['2'].MouseButton1Up:Connect(function()
+							if Thread then
+								task.cancel(Thread)
+								Thread = nil
+							end
+						end))
+					end)
+				end
+			else
+				if Thread then
+					task.cancel(Thread)
+					Thread = nil
+				end
+			end
+		end,
+		Tooltip = 'Hold attack button to automatically click'
+	})
+	CPS = AutoClicker:CreateTwoSlider({
+		Name = 'CPS',
+		Min = 1,
+		Max = 9,
+		DefaultMin = 7,
+		DefaultMax = 7
+	})
+	AutoClicker:CreateToggle({
+		Name = 'Place Blocks',
+		Default = true,
+		Function = function(callback)
+			if BlockCPS.Object then
+				BlockCPS.Object.Visible = callback
+			end
+		end
+	})
+	BlockCPS = AutoClicker:CreateTwoSlider({
+		Name = 'Block CPS',
+		Min = math.huge,
+		Max = math.huge,
+		DefaultMin = math.huge,
+		DefaultMax = math.huge,
+		Darker = true
+	})
+	
+	-- Add the no placement CPS option
+	PlacementCPS = {
+		BLOCK_PLACE_CPS = math.huge -- Set to math.huge to disable placement CPS limit
+	}
 end)
