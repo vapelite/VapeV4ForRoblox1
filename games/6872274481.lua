97066414373cc096904136c5ba92d723ec5449da
vapelite <yhits10inches@gmail.com>
2025-03-04 22:08:12 -0600

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -2366,7 +2366,6 @@ run(function()
 								if actualRoot then
 									local dir = CFrame.lookAt(selfpos, actualRoot.Position).LookVector
 									local pos = selfpos + dir * math.max(delta.Magnitude - 14.399, 0)
-									bedwars.SwordController.lastAttack = workspace:GetServerTimeNow()
 									store.attackReach = (delta.Magnitude * 100) // 1 / 100
 									store.attackReachUpdate = tick()
 									AttackRemote:FireServer({
